6324bed4e3498ada372a284cb79314192d49dde0
fixitsteve <fixitsteve@yahoo.com>
2024-05-20 14:11:39 -0700

    week 2 first commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 var src_default = {
   async fetch(request, env, ctx) {
-    if(request.method == 'post') {
-           return new Response('Hello worker!', {
+    if(request.method == "POST") {
+           const workerResponseMap = {
+            1: "Random Response 1",
+            2: "Random Response 2",
+            3: "Random Response 3",
+            4: "Random Response 4",
+            };
+
+            function getRandomNumber(min, max) {
+                const minCeiled = Math.ceil(1);
+                const maxFloored = Math.floor(4);
+                return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+            };
+
+           return new Response(workerResponseMap[getRandomNumber()], {
                headers: {
                    'content-type': 'application/json',
                },
            });
        }
        else{
-           return new Response('Error Worker! Wrong Method!', {
+           return new Response('Error Worker! Wrong URL', {
                headers: {
                    'content-type': 'text/plain',
                },
@@ -29,5 +33,3 @@ var src_default = {
 export {
   src_default as default
 };
-
-
